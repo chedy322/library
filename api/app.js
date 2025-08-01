6453344bcef784196000be97c3b4021e915ed870
Chadi ben bououn <142152525+chedy322@users.noreply.github.com>
2025-08-01 17:14:19 +0100

    Update app.js

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const app=express()
 const cors=require('cors')
 require('dotenv').config()
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://frontend-library-2.onrender.com/',
     credentials:true
 }))
 const port=process.env.PORT||3500
