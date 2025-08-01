cd1e0a68bbfd12b5b4c3892c34d8906184e4cc85
Chadi ben bououn <142152525+chedy322@users.noreply.github.com>
2025-08-01 16:43:33 +0100

    Update dboperation.jsx

--- my-app/src/utils/dboperation.jsx
+++ my-app/src/utils/dboperation.jsx
@@ -1,5 +1,5 @@
 // import { useState } from "react"
-const url="http://localhost:3500/libraries"
+const url="https://library-3-1tsv.onrender.com/libraries"
 // function to post request
 export const create_library=async(form,setError)=>{
 try{
